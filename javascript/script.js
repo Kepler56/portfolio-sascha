a399bd50a6300db86c6c2ea06824904829403fc8
Sascha Cauchon <98602898+Kepler56@users.noreply.github.com>
2023-12-22 07:15:41 +0100

    Changes on the navigation menu

--- javascript/script.js
+++ javascript/script.js
@@ -1,41 +1,4 @@
-/* We are targetting the menu-links and hamburger-icon elements, so that when the user clicks
-on the hamburger menu it's going to either add or remove the open class*/
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
-
 // Open the PDF URL in a new tab or window
-function openPDF() {
+function openPDF_EN() {
     window.open('CV/CV_English.pdf', '_blank');
 }
-
-//Send an email through the contact-me section
-function sendEmail(){
-    Email.send({
-        SecureToken : "cb5fd779-2ea0-4220-87e0-df1e876dc2b8",
-        To : 'saschacauchon56@gmail.com',
-        From : document.getElementById('email').value,
-        Subject : "New Contact Enquiry",
-        Body : "Name: " + document.getElementById('name').value
-        + "<br> Email: " + document.getElementById('email').value 
-        + "<br> Message: " + document.getElementById('message').value
-    }).then(
-      message => alert("Message sent succesfully")
-    );
-}
-
-// change the styles of just the clicked anchor tag --> not working find solution
-
-const links = document.querySelectorAll(".my-nav-link");
-const sections = document.querySelectorAll("section");
-
-links.forEach((tab, index) => {
-    tab.addEventListener("click", () => {
-        links.forEach(tab => tab.classList.remove("active"));
-        tab.classList.add("active");
-    });
-});
